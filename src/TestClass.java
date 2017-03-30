74d9be331de457bd11e885a3fa7697e19e0e2ed2
Jenny Treanor <jtreanor@cct.ie>
2017-03-30 10:39:58 +0100

    skip staging

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 		System.out.println("hello");
 		System.out.println("Something else");
 		System.out.println("Yet more changes");
-		
+		System.out.println("Final change");
 	}
 }
