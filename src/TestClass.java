737ad522d3d3cad2bae44ce1d193f5c41566d93a
Jenny Treanor <jtreanor@cct.ie>
2017-03-30 10:32:08 +0100

    Add multiple changes over multiple adds, one commit for all.

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Something else");
+		System.out.println("Yet more changes");
+		System.out.println("Final change");
 	}
 }
